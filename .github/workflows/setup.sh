702c3dda7d051596a9d8fad978e5715e6f7bc08c
tdayris <thibault.dayris@outlook.fr>
2019-12-20 10:32:20 +0100

    [test] (CI): Initial commit

---
+++ .github/workflows/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+export SINGULARITY_VER=2.4.5
+
+if type conda > /dev/null; then exit 0; fi
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p miniconda
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+conda env create --file envs/workflow.yaml
